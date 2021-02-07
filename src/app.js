e885c307c3aa71a71439f63b6e7d968e7908e868
jayesquivel <68364021+jayesquivel@users.noreply.github.com>
2021-02-07 14:14:39 -0800

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+import reactDom from 'react-dom';
+
+import './app.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="9d11a91d-a9e6-4cf3-8306-b7bf8506076a"
+        userName="JavascriptMastery"
+        userSecert="123123"
+    />
+    );
+}
+export default App;
